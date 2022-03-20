f5774f552b72003abdcd9816bc92512ad4ae7f8f
Kyungsuh Park <pyo05360@gmail.com>
2022-03-20 23:54:35 +0900

    Add: 기본 세팅 및 부트스트랩 불러오기

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React from 'react';
+import {Navbar, Container, Nav, NavDropdown, Jumbotron, Button  } from 'react-bootstrap';
+import { useState } from 'react';
+// components
+import Data from './data.js';
+
 import './App.css';
 
 function App() {
+  let [shoes, shoes변경] = useState(Data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+
+    <Jumbotron className='background'>
+      <h1>Hello, world!</h1>
+      <p>
+        This is a simple hero unit, a simple jumbotron-style component for calling
+        extra attention to featured content or information.
+      </p>
+      <p>
+        <Button bsStyle="primary">Learn more</Button>
+      </p>
+    </Jumbotron>;
+
+
+    <div className="container">
+        <div className="row">
+          <Card shoes={shoes[0]}/>
+          <Card shoes={shoes[1]}/>
+          <Card shoes={shoes[2]}/>
+        </div>
+    </div>   
+    </>
+    );
+  }
+
+  function Card(props) {
+    return (
+      <div className="col-md-4">
+      <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
+      <h4> {props.shoes.title} </h4> 
+      <p> {props.shoes.content} & {props.shoes.price} </p>
     </div>
-  );
-}
+    )
+  }
+
 
 export default App;
---
+++ src/Shoes.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function shoes() {
+  return (
+    <>
+      <div className="col-md-4">
+        <img
+          src="https://codingapple1.github.io/shop/shoes1.jpg"
+          width="100%"
+        />
+        <h4> 신발이름</h4>
+        <p> 상품설명</p>
+      </div>
+    </>
+  );
+}
+
+export default shoes;
